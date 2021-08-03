010258a62f700eb2e5530b1247ce5077495cc405
icaro bruno <icarobruno@icaros-MacBook-Pro.local>
2021-08-03 00:22:41 -0300

    Vtex info section

---
+++ src/components/VtexInfo/VtexInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Title, Span, Text} from './Styles'
+
+
+export default function VtexInfo() {
+    return (
+        <Container>
+            <Title>Essa loja foi construída usando uma das nossas soluções da plataforma VTEX. tenha a sua.</Title>
+            <Span>Entre em contato</Span>
+            <Text>comercial@jussi.com.br</Text>
+        </Container>
+    )
+}
